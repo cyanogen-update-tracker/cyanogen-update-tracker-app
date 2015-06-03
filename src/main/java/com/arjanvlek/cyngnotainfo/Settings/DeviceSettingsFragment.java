22b371b8c937d9b17bd99553694cd297a2d80272
Arjan Vlek <arjan1995@gmail.com>
2015-06-03 09:44:30 +0200

    fixed onItemClicked in devicesettingsfragment

--- src/main/java/com/arjanvlek/cyngnotainfo/Settings/DeviceSettingsFragment.java
+++ src/main/java/com/arjanvlek/cyngnotainfo/Settings/DeviceSettingsFragment.java
@@ -4,7 +4,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -12,25 +11,20 @@ import android.app.AlertDialog;
 import android.widget.ArrayAdapter;
 
 import com.arjanvlek.cyngnotainfo.MainActivity;
-import com.arjanvlek.cyngnotainfo.Model.DeviceTypeEntity;
 import com.arjanvlek.cyngnotainfo.R;
-import com.arjanvlek.cyngnotainfo.Support.ServerConnector;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class DeviceSettingsFragment extends DialogFragment {
 
     private SharedPreferences sharedPreferences;
-    private List<String>deviceNames = new ArrayList<>();
     private int itemClicked;
-    private AlertDialog alertDialog;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_single_choice, getArguments().getStringArrayList("devices"));
+        final ArrayList<String> devices = getArguments().getStringArrayList("devices");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_single_choice, devices);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.choose_device_type)
                 .setSingleChoiceItems(adapter, 0, new DialogInterface.OnClickListener() {
@@ -45,7 +39,7 @@ public class DeviceSettingsFragment extends DialogFragment {
                     public void onClick(DialogInterface dialog, int which) {
                         sharedPreferences = getActivity().getPreferences(Context.MODE_APPEND);
                         SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString(MainActivity.PROPERTY_DEVICE_TYPE, deviceNames.get(itemClicked));
+                        editor.putString(MainActivity.PROPERTY_DEVICE_TYPE, devices.get(itemClicked));
                         editor.apply();
 
                     }
