c2cd305eea499800dd0714f97914e71839bd028e
Arjan Vlek <arjan1995@gmail.com>
2015-06-24 23:12:33 +0200

    - Added new Google Cloud Messaging (GCM) API - Auto update GCM registration during app upgrades - Renamed update types to update methods

--- src/main/java/com/arjanvlek/cyngnotainfo/GcmPackageReplacedReceiver.java
+++ src/main/java/com/arjanvlek/cyngnotainfo/GcmPackageReplacedReceiver.java
@@ -9,20 +9,16 @@ public class GcmPackageReplacedReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        if (intent != null && (intent.getAction().equals(Intent.ACTION_PACKAGE_REPLACED) || intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED))) {
-            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-                if (intent.getData().getSchemeSpecificPart().equals(context.getPackageName())) {
-                    // invalidate the current GCM registration id, and re-register with GCM server using the GcmRegistrationIntentService
-                    Intent i = new Intent(context, GcmRegistrationIntentService.class);
-                    i.putExtra("package_upgrade", true);
-                    startWakefulService(context, i);
-                }
-                else {
-                    Intent i = new Intent(context, GcmRegistrationIntentService.class);
-                    i.putExtra("package_upgrade", true);
-                    startWakefulService(context, i);
-                }
-            }
+        if (intent != null && intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
+            // invalidate the current GCM registration id, and re-register with GCM server using the GcmRegistrationIntentService
+            Intent i = new Intent(context, GcmRegistrationIntentService.class);
+            i.putExtra("package_upgrade", true);
+            startWakefulService(context, i);
+        }
+        else {
+            Intent i = new Intent(context, GcmRegistrationIntentService.class);
+            i.putExtra("package_upgrade", true);
+            startWakefulService(context, i);
         }
     }
 }
